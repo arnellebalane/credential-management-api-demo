bac0bb23f11caac0157c7f0f5b917aac41094ee8
Arnelle Balane <arnellebalane@gmail.com>
2017-08-26 20:24:42 +0800

    persist user session in localstorage

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
+const user = localStorage.getItem('user');
+if (user) {
+    signIn(JSON.parse(user));
+}
+
+
+
+if (navigator.credentials && navigator.credentials.preventSilentAccess) {
+    (async () => {
+
+        if (!user) {
+            const credentials = await navigator.credentials.get({ password: true });
+            if (credentials) {
+                console.log(credentials);
+            }
+        }
+
+    })();
+}
+
+
+
 $('form').addEventListener('submit', (e) => {
     e.preventDefault();
     const form = e.target;
     const username = form.username.value;
     const password = form.password.value;
 
-    signIn(username, password);
+    const credentials = new PasswordCredential({
+        id: username,
+        password: password
+    });
+    signIn(credentials);
 
     form.username.value = '';
     form.password.value = '';
@@ -15,19 +41,29 @@ $('.profile a').addEventListener('click', (e) => {
     signOut();
 });
 
-function signIn(username, password) {
-    $('.profile h3').textContent = '';
-    $('.profile p').textContent = username;
+async function signIn(credentials) {
+    $('.profile img').src = credentials.iconURL || 'avatar.png';
+    $('.profile h3').textContent = credentials.name || '';
+    $('.profile p').textContent = credentials.id || '';
 
+    const user = only(credentials, ['id', 'name', 'password', 'iconURL']);
+    localStorage.setItem('user', JSON.stringify(user));
     screen('profile');
 }
 
 function signOut() {
+    localStorage.removeItem('user');
     screen('signin');
 }
 
 function $(selector, context = document) {
     return context.querySelector(selector);
+}
+
+function only(object, keys) {
+    return keys.reduce((filtered, key) => (
+        Object.assign(filtered, { [key]: object[key] })
+    ), {});
 }
 
 function screen(name) {
