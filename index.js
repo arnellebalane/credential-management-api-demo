8a195f8aa229c38ca4523dfc75c4454526c2a65a
Arnelle Balane <arnellebalane@gmail.com>
2017-08-26 19:47:27 +0800

    signin/signout screen navigation

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 $('form').addEventListener('submit', (e) => {
+    e.preventDefault();
     const form = e.target;
     const username = form.username.value;
     const password = form.password.value;
+
+    signIn(username, password);
+
+    form.username.value = '';
+    form.password.value = '';
 });
+
+$('.profile a').addEventListener('click', (e) => {
+    e.preventDefault();
+    signOut();
+});
+
+function signIn(username, password) {
+    $('.profile h3').textContent = '';
+    $('.profile p').textContent = username;
+
+    screen('profile');
+}
+
+function signOut() {
+    screen('signin');
+}
 
 function $(selector, context = document) {
     return context.querySelector(selector);
+}
+
+function screen(name) {
+    $(`section[id="${name}"]`).classList.remove('hidden');
+    $(`section:not([id="${name}"])`).classList.add('hidden');
 }
